8505c474c6ceab3cd312f064a017dc30f7558567
Nguyen Ngo Cao Cuong <cuong35228102000@gmail.com>
2021-03-31 23:13:50 +0700

    test

---
+++ js/home.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.menu-control-item .menu-control-prev').click(function() {
+        console.log(this);
+        console.log();
+    });
+    $('.menu-control-item .menu-control-next').click(function() {
+        console.log(this);
+    });
+    console.log($('.img-sheft.text-center .img'));
+    // $('.img-sheft.text-center .img')[0] /
+})
